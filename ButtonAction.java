dcf43aaeaa433f56de35c10b392a6c7dbcca89e9
DefRect <43281438+DefRect@users.noreply.github.com>
2020-12-04 03:53:55 +0300

    Four task

---
+++ ButtonAction.java
@@ -0,0 +1,11 @@
+package lessonFour;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ButtonAction implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Check");
+    }
+}
